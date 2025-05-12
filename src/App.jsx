103bef251f38bc86919caabc1e8a3569309267c9
Sharan Suri <sharansuri-8@hotmail.com>
2025-05-12 12:25:12 +0530

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SignIn from "./pages/SignIn"
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import WelcomePage from "./pages/WelcomePage";
 import ResultPage from "./pages/ResultPage";
 import SettingsPage from "./pages/SettingsPage";
 import MealTracker from "./pages/MealTracker";
 import NutritionAnalytics from "./pages/NutritionAnalytics";
+
 function App() {
-return (
-  <Router>
-    <Routes>
-      <Route path="/Home" element={<HomePage/>}/>
-      <Route path="/SignUp" element={<SignUp/>}/>
-      <Route path="/SignIn" element={<SignIn/>}/>
-      <Route path="/WelcomePage" element={<WelcomePage/>}/>
-      <Route path="/ResultPage" element={<ResultPage/>}/>
-      <Route path="/SettingsPage" element={<SettingsPage/>}/>
-      <Route path="/MealTracker" element={<MealTracker/>}/>
-      <Route path="/NutritionAnalytics" element={<NutritionAnalytics/>}/>
-
-
-
-    </Routes>
-  </Router>
-)
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate to='/Home' replace />} />
+        <Route path='/Home' element={<HomePage />} />
+        <Route path='/SignUp' element={<SignUp />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/WelcomePage' element={<WelcomePage />} />
+        <Route path='/ResultPage' element={<ResultPage />} />
+        <Route path='/SettingsPage' element={<SettingsPage />} />
+        <Route path='/MealTracker' element={<MealTracker />} />
+        <Route path='/NutritionAnalytics' element={<NutritionAnalytics />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
